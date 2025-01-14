f4945e40ba5ab90cebd8018b8dd1410d140fdfdb
sonu28gpt <98702109+sonu28gpt@users.noreply.github.com>
2025-01-14 17:34:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 function firstWord(s) {
   // your code here
+	if(s.trim()=='')return '';
+
+	s=s.trim();
+	let i=0;
+	let temp='';
+	while(i<s.length){
+		if(s[i]==' ')return temp;
+		temp+=s[i];
+		i++;
+	}
+	return temp;
 }
 
-// Do not change the code below
+// Do not change the  code below
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
